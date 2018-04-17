4abcaaedfc445b1a481f135cc72d1ab6d7eaa5d8
D7703_03 <d7703_03@D7703-03ui-iMac.local>
2018-04-17 14:59:10 +0900

    add alert controller

--- UIAlertController/ViewController.swift
+++ UIAlertController/ViewController.swift
@@ -12,14 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btPressed(_ sender: Any) {
+        let myAlert = UIAlertController(title: "알림", message: "설정된 시간이 되었습니다!", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: { (myAction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.red
+            })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {(myAction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.white
+        })
+        
+        let testAction = UIAlertAction(title: "Delete", style: .destructive, handler: {(myAction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.green
+            })
+            
+        myAlert.addAction(okAction)
+        myAlert.addAction(cancelAction)
+        myAlert.addAction(testAction)
+        present(myAlert, animated: true, completion: nil)
     }
-
-
 }
 
